724f7bfc235fada9683dea8f1722021301d45c4b
jaemin-123 <gjaemin4189@gmail.com>
2025-11-18 20:55:23 +0900

    Add files via upload

---
+++ examples/06.decoder/tb_decoder.v
@@ -0,0 +1,17 @@
+`timescale 1ns/1ps
+module tb_decoder;
+  reg [3:0] iIN;
+  wire [9:0] oOUT;
+  integer i;
+  decoder U0 (
+  .iIN (iIN), 
+  .oOUT (oOUT));
+  initial begin
+    iIN = 0;
+    for (i = 0; i<=10; i = i + 1) begin
+        #100 iIN = iIN + 1;
+    end
+    $finish;
+  end
+
+endmodule
